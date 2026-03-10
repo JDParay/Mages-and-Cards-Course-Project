30915cd6b684ff1371a7f9365b266b734f80d0fa
Jeko Paray <jdparay@mcm.edu.ph>
2026-03-10 18:07:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleMenu(destination) {
+  console.log("Navigating to: " + destination);
+  
+  if(destination === 'START') {
+    alert("Loading Chapter Select...");
+    // This is where you'd swap the HTML to show your Chapter Select screen
+  }
+}
