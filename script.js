062cc0baa6388c8820a72adf17e41514ec26dd42
Jeko Paray <jdparay@mcm.edu.ph>
2026-03-10 19:42:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function handleMenu(destination) {
     // This is where you'd swap the HTML to show your Chapter Select screen
   }
 }
+
+function openOptions() {
+    document.body.classList.add('modal-active');
+    document.getElementById('options-modal').classList.add('show');
+}
+
+function openCredits() {
+    document.body.classList.add('modal-active');
+    document.getElementById('credits-modal').classList.add('show');
+}
+
+function closeModals() {
+    document.body.classList.remove('modal-active');
+    document.querySelectorAll('.modal-box').forEach(m => m.classList.remove('show'));
+}
